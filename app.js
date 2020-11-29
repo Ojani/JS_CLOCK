d6c2c3cc6a7fd4077f4c653ac6426905f92e080c
Ojani Figueroa <ojanifigueroa.of@gmail.com>
2020-11-29 19:49:07 -0400

    Added finished clock.

---
+++ app.js
@@ -0,0 +1,42 @@
+const hrArm = document.querySelector(".hr_arm");
+const minArm = document.querySelector(".min_arm");
+const secArm = document.querySelector(".sec_arm");
+
+// This function looks at the time contantly in order to start counting the seconds as soon as a real second changes so that it is accurate within at least 50 milliseconds
+
+function getTime(oldTime = new Date().getTime()) {
+    const newTime = new Date().getTime();
+    const timeDiff = newTime - oldTime;
+
+    if(timeDiff < 50) {
+        countTime(newTime);
+        return;
+    }
+
+    setTimeout(() => { getTime()}, 0);
+}
+
+// Updating the clock
+function countTime() {
+
+    const date = new Date();
+
+    const hours = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes() + 1;
+    const seconds = date.getSeconds() + 1;
+
+    const hrPos = 360 / 12 * hours;
+    const minPos = 360 / 60 * minutes;
+    const secPos = 360 / 60 * seconds;
+
+    hrArm.style.transform = `translate(-50%, -100%) rotate(${ hrPos }deg)`;
+    minArm.style.transform = `translate(-50%, -100%) rotate(${ minPos }deg)`;
+    secArm.style.transform = `translate(-50%, -100%) rotate(${ secPos }deg)`;
+
+    // Calling this function again in exactly one second
+    setTimeout(() => {
+        countTime();
+    }, 1000);
+}
+
+getTime();
